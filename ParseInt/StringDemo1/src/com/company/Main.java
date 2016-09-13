5e7db393d6812288091d9ae24b8b5febf837c149
Shoukat Malik <you@example.com>
2016-09-13 07:39:47 -0700

    Adding files to repository

---
+++ ParseInt/StringDemo1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*String value="1000";
+        double result=Double.parseDouble(value);
+        System.out.println(result);
+        System.out.println(result+1);
+
+    */
+        int number = 100;
+        String result=Integer.toString(number);
+        System.out.println(result);
+    }
+}
